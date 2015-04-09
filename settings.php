d65cfd13ff926b4ed69a8da2fa9c334ee211a084
neswill1 <nenadkonatar@live.ac.me>
2015-04-09 13:57:55 +0200

    First commit

---
+++ settings.php
@@ -0,0 +1,8 @@
+<?php
+$adresa="953 3rd ave north 953 3RD ave north and 41 naples fl 34102, Naples FL 34102"; //jako je vazno navesti dobro adresu radi boljeg poziciniranja
+$grad="Naples";//jako je vazno ako adresa ima dva imena povezati ih sa plus
+$skracenica=""; // AKo ne znate skracenicu ostavite praznu
+$telephone= 2396012700;// Broj koji se poziva na dugme CALL US
+$theme="b";//Thema koja se aplicira na sve
+$transitionefect="flip"; //kucate tranziciju koju zelite na stranama . Vrste tranzicija : "fade", "flip", "pop", "slide", "slidedown", "slideup"
+?>
